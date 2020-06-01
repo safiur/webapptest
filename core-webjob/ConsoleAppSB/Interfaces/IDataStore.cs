ca932af484c55c1d94508f47623b7df662e5eab9
safiur <safiur.rehman@equinox.com>
2020-06-01 10:03:24 +0530

    hghghghgh

---
+++ core-webjob/ConsoleAppSB/Interfaces/IDataStore.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleAppSB.Interfaces
+{
+	public interface IDataStore
+	{
+		void ProcessData();
+	}
+}
---
+++ core-webjob/ConsoleAppSB/Services/DataStore.cs
@@ -0,0 +1,15 @@
+﻿using ConsoleAppSB.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleAppSB.Services
+{
+	public class DataStore : IDataStore
+	{
+		public void ProcessData()
+		{
+			Console.WriteLine("Processing data...");
+		}
+	}
+}
